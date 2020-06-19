4452977dd8e381ef6a29da2df6b3aa7d5145718f
ep3824 <ethan.parent1@gmail.com>
2020-06-18 20:29:06 -0600

    create star ratings

--- client/src/components/StarRatings.jsx
+++ client/src/components/StarRatings.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
-
+import StarRating from './StarRating.jsx';
 
 class StarRatings extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      something: 'hasValue',
-    };
+    this.state = [
+      { name: 'sony', stars: 4.7 },
+      { name: 'samsung', stars: 3.4 },
+      { name: 'vizio', stars: 2.3 },
+      { name: 'panasonic', stars: 3.6 },
+      { name: 'philips', stars: 4.1 },
+    ];
   }
 
   render() {
-    const { something } = this.state;
     return (
-      <div>
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>4k Television</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.map((product) =>
+              <StarRating 
+                name={product.name}
+                rating={product.stars}
+              />
+              )}
+          </tbody>
+        </table>
         <h1>STARS!</h1>
-        <h2>{something}</h2>
       </div>
     );
   }
