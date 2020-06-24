d030333b84c601bce810f248df8544cc9c1ba88d
ep3824 <ethan.parent1@gmail.com>
2020-06-24 11:09:49 -0600

    minor css refactoring + app refactoring

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Review from './Review.jsx';
+import Sort from './Sort';
 
 class ReviewList extends React.Component {
   constructor(props) {
@@ -11,20 +12,21 @@ class ReviewList extends React.Component {
   render() {
     return (
       <div className="container">
+        <Sort />
         <div className="table table-striped">
-            {this.props.reviewData.map((product, i) =>
-              <Review
-                key={i} 
-                name={product.reviewer_name}
-                rating={product.rating}
-                summary={product.summary}
-                body={product.body}
-                date={product.date}
-                helpfulness={product.helpfulness}
-                recommend={product.recommend}
-                reviewData={this.props.reviewData}
-              />
-              )}
+          {this.props.reviewData.map((product, i) =>
+            <Review
+              key={i} 
+              name={product.reviewer_name}
+              rating={product.rating}
+              summary={product.summary}
+              body={product.body}
+              date={product.date}
+              helpfulness={product.helpfulness}
+              recommend={product.recommend}
+              reviewData={this.props.reviewData}
+            />
+          )}
         </div>
       </div>
     );
