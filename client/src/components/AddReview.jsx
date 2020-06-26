49e8381fbe8b56c79fa0012cf182655a1b2b92b2
ep3824 <ethan.parent1@gmail.com>
2020-06-26 13:01:44 -0600

    add additional input fields with styling

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -28,19 +28,36 @@ class AddReview extends React.Component {
               </button>
             </div>
             <div className="modal-body">
+              <div className="modal-star-rating">Rate the product 1-5:</div>
+              <div className="modal-recommend">Would you recommend this produdct?</div>
+
               <AddReviewTable />
-              {/* table goes here */}
-              {/* another chekcbox */}
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="defaultCheck2" disabled></input>
-                  <label className="form-check-label" htmlFor="defaultCheck2">
-                    Disabled checkbox
-                  </label>
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">Summary</span>
+                </div>
+                <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></input>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlTextarea1">Review Message:</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">Nickname</span>
+                </div>
+                <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></input>
+              </div>
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">Email</span>
+                </div>
+                <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></input>
               </div>
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+              <button type="button" className="btn btn-primary">Submit review</button>
             </div>
           </div>
         </div>
