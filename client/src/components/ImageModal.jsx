1e0b554d1c2b4e67c374a341c78c1bcea6a0feb2
ep3824 <ethan.parent1@gmail.com>
2020-07-01 17:11:00 -0600

    fix image modals

--- client/src/components/ImageModal.jsx
+++ client/src/components/ImageModal.jsx
@@ -4,31 +4,40 @@ class ImageModal extends Component {
   constructor(props) {
     super(props);
 
-    this.state = null;
+    this.state = {
+      modalVisibility: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState(prevState => ({
+      modalVisibility: !prevState.modalVisibility,
+    }));
   }
 
   render() {
     return (
       <div>
-        <input type="image" width="20%" height="20%" src={this.props.photo} name="modalImageButton" className="image-modal-button" data-toggle="modal" data-target="#exampleModal"/>
-        <div className={`modal fade bd-example-modal-lg`} id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog modal-lg" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Photos</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <img width="100%" height="100%" src={this.props.photo}></img>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input type="image" width="20%" onClick={this.onClick} height="20%" src={this.props.photo} name="modalImageButton" className="image-modal-button" data-toggle="modal" data-target={`#modal${this.props.id}`} />
+          <div className='modal fade bd-example-modal-lg' id={`modal${this.props.id}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog modal-lg" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Photos</h5>
+                  <button onClick={this.onClick} type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <img width="100%" height="100%" src={this.props.photo}></img>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
       </div>
     );
   }
