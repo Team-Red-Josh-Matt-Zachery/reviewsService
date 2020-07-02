3e0d3c5706f0a7c890dbbe25e9d149935ec5ae55
ep3824 <ethan.parent1@gmail.com>
2020-07-02 13:36:51 -0600

    allow chevrons to move accross the page at a variable amount

--- client/src/components/AttributeGraph.jsx
+++ client/src/components/AttributeGraph.jsx
@@ -5,37 +5,60 @@ class AttributeGraph extends Component {
     super(props);
 
     this.state = {
-      barPercents: ['50%', '50%', '50%', '50%', '50%', '50%', '50%'],
+      size: '',
+      width: '',
+      comfort: '',
+      quality: '',
+      length: '',
+      fit: '',
     };
   }
 
+  componentDidMount() {
+    fetch('http://52.26.193.201:3000/reviews/102/meta')
+      .then(res => res.json()) 
+      .then(data => this.createGraphs(data));
+  }
+
+  //Come back to***
+
+  createGraphs(data) {
+    let dataKeys = Object.keys(data.characteristics);
+    let stateKeys = Object.keys(this.state)
+    for (let i = 0; i < dataKeys.length; i++) {
+      if (stateKeys.includes(dataKeys[i].toLowerCase())) {
+        let widthPercent = ((Number(data.characteristics[dataKeys[i]].value) / 5) * 100)
+        widthPercent = `${widthPercent}%`
+        this.setState({
+          [dataKeys[i].toLowerCase()]: widthPercent,
+        });
+      }
+    }
+  }
+
   render() {
-    let { barPercents } = this.state
+    let { size, width, comfort, quality, length, fit } = this.state;
+    let stateKeys = Object.keys(this.state);
     return (
         <div className="attribute-graph">
-        <div className="attr-outer">
-          {/* <div className="attr-breakdown" style={{width: barPercents[4]}}></div> */}
-          <img className="attr-breakdown" width="50%" height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
-        </div>
-        <div className="attr-outer">
-          {/* <div className="attr-breakdown" style={{width: barPercents[3]}}></div> */}
-          <img width="5%" height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
-        </div>
-        <div className="attr-outer">
-          {/* <div className="attr-breakdown" style={{width: barPercents[2]}}></div> */}
-          <img width="5%" height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
-
-        </div>
-        <div className="attr-outer">
-          {/* <div className="attr-breakdown" style={{width: barPercents[1]}}></div> */}
-          <img width="5%" height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
-
-        </div>
-        <div className="attr-outer">
-          {/* <div className="attr-breakdown" style={{width: barPercents[0]}}></div> */}
-          <img width="5%" height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
-
-        </div>
+          <div className="attr-outer">
+            <img className="attr-breakdown-size" style={{left: size}} height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
+          </div>
+          <div className="attr-outer">
+            <img className="attr-breakdown-width" style={{left: width}} height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
+          </div>
+          <div className="attr-outer">
+            <img className="attr-breakdown-comfort" style={{left: comfort}} height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
+          </div>
+          <div className="attr-outer">
+            <img className="attr-breakdown-quality" style={{left: quality}} height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
+          </div>
+          <div className="attr-outer">
+            <img className="attr-breakdown-length" style={{left: length}} height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
+          </div>
+          <div className="attr-outer">
+            <img className="attr-breakdown-fit" style={{left: fit}} height="5%" src="https://i.ya-webdesign.com/images/vector-triangles-upside-down-2.png"></img>
+          </div>
       </div>
     );
   }
