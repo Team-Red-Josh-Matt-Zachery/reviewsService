b4d68a5b84a1528192ea4236630ce8fe5cb9ff12
Zachery Converse <zacheryconverse@gmail.com>
2020-07-08 18:13:40 -0600

    Improve db server and backend server.

--- database/index.js
+++ database/index.js
@@ -13,55 +13,55 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'DB connection error: '));
 
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
 
-// let listSchema = new Schema({
-//   product_id: Integer,
-//   page: Number,
-//   count: Number,
-//   sort: String,
-// })
+// // let listSchema = new Schema({
+// //   product_id: Integer,
+// //   page: Number,
+// //   count: Number,
+// //   sort: String,
+// // })
 
-// let List = mongoose.model('List, listSchema');
+// // let List = mongoose.model('List, listSchema');
 
-const reviewsSchema = new Schema({
-  // product_id: Number,
-  review_id: Number,
-  rating: Number,
-  summary: String,
-  recommend: Boolean,
-  response: String,
-  body: String,
-  date: String,
-  reviewer_name: String,
-  helpfulness: Number,
-  photos: [{ id: String, url: String }], // [{type: String}]
-  // photos: [{
-  //   type: Object,
-  //   properties: {
-  //     id: String,
-  //     url: String,
-  //   }
-  // }],
-  name: String,
-  email: String,
-  // characteristics: Object,
-  characteristics: {
-    Size: {
-      value: String,
-    },
-    Width: {
-      value: String,
-    },
-    Comfort: {
-      value: String,
-    },
-  },
-});
+// const reviewsSchema = new Schema({
+//   // product_id: Number,
+//   review_id: Number,
+//   rating: Number,
+//   summary: String,
+//   recommend: Boolean,
+//   response: String,
+//   body: String,
+//   date: String,
+//   reviewer_name: String,
+//   helpfulness: Number,
+//   photos: [{ id: String, url: String }], // [{type: String}]
+//   // photos: [{
+//   //   type: Object,
+//   //   properties: {
+//   //     id: String,
+//   //     url: String,
+//   //   }
+//   // }],
+//   name: String,
+//   email: String,
+//   // characteristics: Object,
+//   characteristics: {
+//     Size: {
+//       value: String,
+//     },
+//     Width: {
+//       value: String,
+//     },
+//     Comfort: {
+//       value: String,
+//     },
+//   },
+// });
 
-const Reviews = mongoose.model('Reviews, reviewsSchema');
+// const Reviews = mongoose.model('Reviews, reviewsSchema');
 
-module.exports = {
-  Reviews,
+// module.exports = {
+//   Reviews,
 
-};
+// };
