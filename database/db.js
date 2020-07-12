79d041e63f06d396bcbb9cb20162992994df8ed3
Zachery Converse <zacheryconverse@gmail.com>
2020-07-11 18:46:01 -0600

    Create csv file. create seeding file. Create preliminary seeding script.

--- database/db.js
+++ database/db.js
@@ -10,19 +10,22 @@ const addReview = (reviewObject) => {
 };
 
 const findAll = (productId) => (
-  Review.find({ product_id: productId })
+  Review
+    .find({ product_id: productId })
     .then((data) => data)
     .catch((err) => console.log('err', err))
 );
 
 const updateReview = (id, reviewObject) => (
-  Review.updateOne({ review_id: id }, reviewObject)
+  Review
+    .updateOne({ review_id: id }, reviewObject)
     .then((data) => data)
     .catch((err) => console.log('Database function Error: ', err))
 );
 
 const deleteReview = (reviewId) => (
-  Review.deleteOne({ _id: reviewId })
+  Review
+    .deleteOne({ _id: reviewId })
     .then((res) => res)
     .catch((err) => console.log('Database function Error: ', err))
 );
