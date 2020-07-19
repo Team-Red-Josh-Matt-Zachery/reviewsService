411aa944c2aba8caa2e28aed645f0117ef227bfe
Zachery Converse <zacheryconverse@gmail.com>
2020-07-19 14:00:29 -0600

    Stress test locally. Mean response time of 939.73ms

--- scenarios.js
+++ scenarios.js
@@ -1,20 +1,13 @@
 const Faker = require('faker');
 
-const getProductId = (userContext, events, done) => {
-  userContext.vars.product_id = Faker.random.number(100000);
-  return done();
-  // const product_id = Faker.random.number(100000);
-  // return product_id;
-};
-
-const getReviewId = (userContext, events, done) => {
-  userContext.vars.review_id = Faker.random.number({ min: 9000000, max: 10000000 });
-  return done();
-  // const review_id = Faker.random.number({ min: 9000000, max: 10000000 });
-  // return review_id;
-};
-
 module.exports = {
-  getProductId,
-  getReviewId,
+  getProductId: (userContext, events, done) => {
+    userContext.vars.product_id = Faker.random.number(100000);
+    return done();
+  },
+
+  getReviewId: (userContext, events, done) => {
+    userContext.vars.review_id = Faker.random.number({ min: 9000000, max: 10000000 });
+    return done();
+  },
 };
